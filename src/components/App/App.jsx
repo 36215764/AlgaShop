02bf1994a3c33f0f094a3df078299f1406fe9110
Richard Guesso <richardguesso57@gmail.com>
2021-09-24 16:11:28 -0300

    Added Linechart Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CheckBox from '../../shared/CheckBox';
+import LineChart from '../../shared/LineChart';
 import AppContainer from '../AppContainer';
 import AppHeader from '../AppHeader';
 import { Container, Wrapper } from './App.styles';
@@ -7,6 +8,15 @@ import { Container, Wrapper } from './App.styles';
 const App = () => {
     const [lettuce, setLettuce] = useState(false)
     const [rice, setRice] = useState(false)
+    const [percentage, setPercentage] = useState(20)
+
+    const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61']
+
+    useEffect(() => {
+        setTimeout(() => {
+            setPercentage(80)
+        }, 5000)
+    }, [])
 
     return ( 
     <Wrapper>
@@ -26,7 +36,30 @@ const App = () => {
                 }
 
                 right={
-                    <div>Estatisticas</div>
+                    <div>
+                        Estatisticas
+
+                        <LineChart 
+                            color={colors[0]}
+                            title="Saudavel"
+                            percentage={percentage}
+                        />
+                        <LineChart 
+                            color={colors[1]}
+                            title="Não tão saudavel assim"
+                            percentage={20}
+                        />
+                        <LineChart 
+                            color={colors[2]}
+                            title="Limpeza"
+                            percentage={35}
+                        />
+                        <LineChart 
+                            color={colors[3]}
+                            title="Outros"
+                            percentage={15}
+                        />
+                    </div>
                 }
             />
         </Container>
