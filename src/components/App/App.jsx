211bcb46f4e3e10a3e69dadf35b3b8d2fe6d3f61
Richard Guesso <richardguesso57@gmail.com>
2021-09-24 11:52:59 -0300

    Added AppHerader Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import AppHeader from '../AppHeader';
 import { Container, Wrapper } from './App.styles';
 
 const App = () => {
     return ( 
     <Wrapper>
         <Container>
+            <AppHeader />
             todo: App
         </Container>
     </Wrapper> 
---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Wrapper } from './AppHeader.styles';
+
+const AppHeader = () => {
+    return <Wrapper>alga <span>Shopping</span></Wrapper>;
+}
+ 
+export default AppHeader;
