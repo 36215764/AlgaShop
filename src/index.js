a57a0647fa89327542199dd481835eb92f7c7a8f
Richard Guesso <richardguesso57@gmail.com>
2021-09-24 10:32:02 -0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return ( <div>Ola, mundo!</div> );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
